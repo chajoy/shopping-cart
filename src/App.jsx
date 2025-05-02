65c3e063a4d24aa529cc3d3689b7618ea5263e70
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2025-05-02 22:33:39 +0100

    Amend navbar responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,27 @@ const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <>
-      <nav className="bg-stone-100 flex h-[100px] gap-20 justify-around items-center sm:justify-center relative">
+    <div className="bg-stone-100">
+      <div className="flex h-[100px] gap-5 justify-end items-center relative p-10 max-w-maxw m-auto sm:justify-around">
+        <h1 className="font-header text-2xl mr-auto sm:absolute sm:translate-x-[-50%] left-[50%]">
+          KickStart
+        </h1>
         <Button to={"/"} style="hidden sm:block">
           Home
         </Button>
-        <h1 className="font-header text-2xl">KickStart</h1>
-        <Button to={"shop"} style="hidden sm:block">
+        <Button to={"shop"} style="hidden sm:block mr-auto">
           Shop
         </Button>
+        <button>
+          <i class="bi bi-bag-fill text-2xl"></i>
+        </button>
         <button
           className="block sm:hidden "
           onClick={() => setMenuOpen((prev) => (prev = !prev))}
         >
           <i className="bi bi-list text-4xl hover:cursor-pointer"></i>
         </button>
+
         <div
           className={`absolute top-full left-0 w-full flex flex-col sm:hidden ${
             menuOpen ? "block" : "hidden"
@@ -42,8 +48,8 @@ const Nav = () => {
             Shop
           </Link>
         </div>
-      </nav>
-    </>
+      </div>
+    </div>
   );
 };
 
@@ -51,7 +57,7 @@ function App() {
   return (
     <>
       <Nav />
-      <div className="m-auto max-w-400">
+      <div className="m-auto max-w-maxw">
         <Outlet />
       </div>
     </>
